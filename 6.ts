11a24052f66c9bdc5cbf0aac96bf5fca1b7a549f
RajibDesai <rajib.desai@gmail.com>
2024-11-29 00:18:24 +0600

    class-constructor

---
+++ 6.ts
@@ -0,0 +1,25 @@
+{
+    //
+
+    interface Profile {
+        name: string;
+        age: number;
+        email: string
+    }
+    
+    const myProfile : Profile = {
+        name: "Alice",
+        age: 25,
+        email: "alice@example.com"
+    };
+    
+    function updateProfile<T>(obj: T, updates: Partial<T>):T { 
+        return {...obj, ...updates}; 
+    }
+    
+    console.log(updateProfile(myProfile, { age: 26 }));
+
+
+
+    //
+}
---
+++ 7.ts
@@ -0,0 +1,24 @@
+{
+    // 
+    
+    class Car {
+        make: string;
+        model: string;
+        year: number
+        constructor(make: string, model: string, year: number) {
+            this.make = make;
+            this.model = model;
+            this.year = year
+        }
+    
+        getCarAge(numOfYear1: number,) {
+            return numOfYear1 - this.year
+        }
+    }
+    
+    const car = new Car("Honda", "Civic", 2018);
+    const res = car.getCarAge(new Date().getFullYear());
+    console.log(res)
+
+    // 
+}
